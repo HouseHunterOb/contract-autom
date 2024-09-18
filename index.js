c1165622ae89014ab43d7acb26726a855fd714a8
Diego Jonguitud G <diegojonguitud@househunter.com.mx>
2024-09-17 20:54:01 -0600

    Add new doc and add few code ti index.js

---
+++ index.js
@@ -0,0 +1,49 @@
+/*   
+Primer párrafo
+{nmeArrendador}
+{nmeRpteArrendador}
+{nmeArrendatario}
+{nmeRpteArrendatario}
+{nmeFiador}
+{obligadoSolidario}
+ {nmeRepteFiadorSolidario}
+ {fiadorSolidario}
+*/
+
+/* 
+
+a)	Constituiva Arrendador: {noEscrituraArrendador} {noLibroArrendador} {dateEscrituraArrendador} {noNotariaArrendador} {dtoNotarialArrendador} {nmeNotarioArrendador}
+
+b) Datos Arrendador {emailArrendador} {telArrendador}.
+
+c)	Que su RFC es {rfcArrendador}.
+
+d)	Uso de propiedad y dirección: {usoPropiedad} {direccionPropiedad}.
+
+e)	Qué servicios: {serviciosPublicos}
+
+h)	En caso de haber anexo de inventario {y en las condiciones e inventario que se detalla mediante el ANEXO adjunto al presente contrato, como parte fundamental del mismo.} {documentoAnexo}.
+
+
+j)	Que su representante legal se identifica: {documentoIdentificacion}{numeroIdentidadArrendador}{dependenciaExpedidora}.
+
+*/
+
+
+//nmeArrendador
+//nmeRpteArrendador
+//nmeArrendatario
+//nmeFiador
+//Credencial de Votar el Instituto Nacional Electoral o Pasaporte la Secretaría de Relaciones Exteriores {noId{} {cdalIne} {pasaporte} {noIdArrendador} {noIdFiador} {noIdArrendador} {ine} {sre}
+//domicilio
+// Datos de contacto: telArrendador emailArrendador
+// {rfcArrendador} {rfcArrendatario} {rfcFiador} {RFC} {CURP} 
+// {tipo de uso, ej: casa-habitación/local comercial/bodega/oficina} {usoSuelo}
+//{addressProp}
+// {AGUA (SUPEDITADA AL SUMINISTRO PÚBLICO), DRENAJE, GAS Y ENERGÍA ELÉCTRICA}
+// En caso de Anexo de inventario {y en las condiciones e inventario que se detalla mediante el ANEXO adjunto al presente contrato, como parte fundamental del mismo.}
+// Datos de Escritura de Poder o Asamblea o Constitutiva {noEscritura} {noLibro} {dateEscritura} {dtoNotarial} {nmeNotario} 
+// {pagoRenta} {pagoRentaEscrito} {centavos} {payDay} 
+// {cuentaBancariaArrendador}{cuentaClabeArrendador}{bancoArrendador}{nmeCuentaBancoArrendador}
+// {interesMoratorio}
+// {En caso de que el inmueble pertenezca a un fraccionamiento o condominio} {Asimismo, "EL ARRENDATARIO" se hace responsable en caso de multa o sanción económica impuesta por la administración del fraccionamiento o condominio debido a conductas que infrinjan el reglamento, ya sea por parte del arrendatario o de sus acompañantes. En tal caso, "EL ARRENDATARIO" autoriza expresamente a retener la cantidad necesaria para el pago de dichas multas o sanciones del depósito entregado al inicio del contrato.}
